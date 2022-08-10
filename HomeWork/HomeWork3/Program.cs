953fc4b7165264d84b618a29202771ea363bf1db
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-10 23:04:41 +0700

    Незакончел упрожнение 3 нашел пример из интернета осталось разобраться

---
+++ HomeWork/HomeWork3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+//int number;
+//Console.Write("Введите число "); 
+//number = Console.ReadLine();
+
+//if ( number % 2 == 1 )
+//{
+    //ConsoleApplication1.writeLine("Число четное");
+//}
+//else 
+//{
+    //ConsoleApplication1.writeLine("Число нечетное");
+//}
+
+//Console.ReadKey();
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;                             
+            Console.Write("введите число: ");
+            a= Console.Read();
+ 
+            if (a % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+ 
+            Console.ReadKey();
+        }
+    }
+}
