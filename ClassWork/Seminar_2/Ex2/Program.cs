8bfe9d002be9de4d5ced0ab6b26686251a5220b5
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-13 20:13:07 +0700

    Задание 2

---
+++ ClassWork/Seminar_2/Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число "); 
+int number1=int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число "); 
+int number2=int.Parse(Console.ReadLine());
+
+if (number1 % number2 == 0 )
+{
+    Console.Write("Кратно"); 
+}
+else 
+{
+    Console.Write($"Не Кратно, остаток {number1 % number2}"); 
+}
