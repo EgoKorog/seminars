00df8568e0f566774636bc2959a763dca82786af
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-13 20:49:02 +0700

    Решение 3 задачи

---
+++ ClassWork/Seminar_2/Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число "); 
+int num=int.Parse(Console.ReadLine());
+
+int num1 = 7;
+int num2 = 23;
+
+if (num % num1 == 0 && num % num2 == 0)
+{
+    Console.WriteLine( "Кратно" );
+    Console.WriteLine($"число {num} кратно {num1} {num2}  ");
+}
+else 
+{
+    Console.WriteLine( "Не кратно");
+}
+
