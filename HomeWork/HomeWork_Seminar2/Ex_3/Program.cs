35a7ec47fd0398d859166697467ac6c3cf476a81
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-14 18:04:58 +0700

    Выполнение Упрожнения 3 Семинара 2

---
+++ HomeWork/HomeWork_Seminar2/Ex_3/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// String [] Weekdays = { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+// int [] Weekdays = { 1, 2, 3, 4, 5, 6, 7 };
+
+// int n = Weekdays.Length;
+Console.Write("Введите число от 1 до 7: -> "); 
+int num = int.Parse(Console.ReadLine());
+
+
+
+// int monday;
+// int tuesday; 
+// int wednesday; 
+// int thursday; 
+// int friday; 
+// int saturday; 
+// int sunday; 
+
+// int monday = 1 ;
+// int tuesday; 
+// int wednesday; 
+// int thursday; 
+// int friday; 
+// int saturday; 
+// int sunday;  
+if (num == 6)
+{
+    Console.WriteLine($" Сегодня Суббота "); 
+    return;
+}
+if (num >= 6)
+{
+    Console.WriteLine($" Сегодня Воскресенье "); 
+    return;
+}
+else 
+{
+    Console.WriteLine($" Топай на работу! "); 
+    return;
+}
+
+
+
+
+
+
+// int Monday = 0;
+// int Tuesday = 1;
+// int Wednesday = 2;
+// int Thursday = 3;
+// int Friday = 4;
+// int Saturday = 5;
+// int Sunday =6;
