a191d620c776dc57c6da61bce16e10161ab38364
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-14 16:12:20 +0700

    Решение второй задачи вторго семинара

---
+++ HomeWork/HomeWork_Seminar2/Ex_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите любое число "); 
+int num = int.Parse(Console.ReadLine());
+
+if (num <= 99)
+{
+    Console.WriteLine($"Смотри что вводишь третьей цифры нет в двухзначном числе {num} "); 
+    return;
+}
+if (num <= 999)
+{
+    int num1 = num % 10;
+    Console.WriteLine($"Третья цифра числа {num} -> {num1}");
+    return;
+}
+if (num <= 9999)
+{
+    int num1 = num % 100; 
+    int num2 = num1 / 10;
+    Console.WriteLine($"Третья цифра числа {num} -> {num2}");
+    return;
+}
+if (num <= 99999)
+{
+    int num1 = num % 1000; 
+    int num2 = num1 / 100;
+    Console.WriteLine($"Третья цифра числа {num} -> {num2}");
+    return;
+}
+else
+{
+    Console.WriteLine($"Число ОЧЕНЬ БОЛЬШОЕ считай сам!");
+    return;
+}
