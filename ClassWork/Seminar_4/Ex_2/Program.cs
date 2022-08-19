ee7ee72dcb8bdcf6f0622f7dc733feff25778567
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-19 18:30:17 +0700

    Решение второй зада четвертого семинара

---
+++ ClassWork/Seminar_4/Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(GetMultiply(num));
+
+int GetMultiply ( int number) //int GetMultiply- метод получения произведения
+{
+    int result = 1; // строка получения результата 
+    for( int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+
+   
+   
+    return result;
+}
