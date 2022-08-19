fb87beb30bbea7ae01007ca394cbd0c194fdf03d
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-19 19:21:26 +0700

    Решение третьей задачи четвертого семинара и решение третьей задачи из ДЗ

---
+++ ClassWork/Seminar_4/Ex_3/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array; // Объявление массива
+array = GetBinaryArray(8); //Создание массива через функцию и  обозначение его длинны
+ 
+
+// Console.WriteLine("Масив из 8 элементов : "); // вывод результата на консоль 1 способ через цикл
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     Console.Write(array[i]);
+// }
+
+Console.WriteLine($"[{string.Join(",", array)}]"); // вывод результата 2 способ в строку
+
+int[] GetBinaryArray( int size) // в самом методе мы создаем и возвращаем массив 
+{
+    int[] arr = new int[size]; // создание цикла и приравневание его к длинне 
+    for(int i =0; i < size; i++) // цикл по заполнению массива 
+    {
+        arr[i] = new Random().Next(100); // условия заполнения цикла 
+    }
+    return arr; // return возвращает массив
+
+} 
