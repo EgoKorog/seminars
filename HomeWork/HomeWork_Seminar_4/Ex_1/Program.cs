f0fa2c7b5d082b16bd1c501d4f71dc3035deacd2
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-19 21:54:12 +0700

    Решение 1 задачи 4 семинара

---
+++ HomeWork/HomeWork_Seminar_4/Ex_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+ 
+using System;
+ 
+class Program {
+ public static int Main() {
+  
+  Console.Write("Введите число   : ");
+  int num = int.Parse(Console.ReadLine());
+ 
+  Console.Write("Введите степень : ");
+  int n = int.Parse(Console.ReadLine());
+  
+  int num_n=1;
+  for(int i=0; i<n; i++) {
+   num_n*=num;
+  }
+  Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
+  Console.ReadKey();
+  return 0;
+ }
+}
+
+// Как решить задачу циклом не разобрался подскажите!
+
+
+
+// double pow = Math.Pow(a,b);
+
+// Console.WriteLine($"число {a} в натуральной степени {b} равно {pow}");
