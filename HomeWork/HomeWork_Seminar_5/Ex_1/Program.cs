d8567161826095d190c1446cff448dbe0925b689
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-28 22:18:01 +0700

    Решение 1 ДЗ 5 семинара

---
+++ HomeWork/HomeWork_Seminar_5/Ex_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int [] array = GetArray(4,100,200);
+
+Console.WriteLine(String.Join(",", array));
+
+Console.WriteLine(String.Join(",", array1(array)));
+
+int[] GetArray(int count, int minValue, int maxValue) // Обьявил условия для массива в методе 1. длина массива 2. мин. значение и макс.значение.
+{
+    int [] array = new int[count]; // объявил массив array и сказал что он будет равен count то есть его длинне.
+
+    for (int i = 0; i < count; i++) // запустил цикл for (для) внес переменные цикла и задал условия  
+    {
+        array[i] = new Random(). Next (minValue, maxValue + 1); // условия заполнения цикла
+    }
+
+    return array; // возврат 
+}
+
+int array1 ( int [] array )
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] %2==0)
+        sum+=1;
+
+    }
+    return sum;
+}
+  
