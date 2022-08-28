1f2921630d8ce2e77fd4b0f96725cdc8437a9500
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-08-28 18:02:59 +0700

    Решение второй задачи 5 семинара

---
+++ ClassWork/Seminar_5/Ex_2/Program.cs
@@ -0,0 +1,32 @@
+﻿int [] array = GetArray(4,-9,9);
+
+Console.WriteLine(String.Join(",", array));
+
+Console.WriteLine("Введите искомое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Число {a} найдено ? : {FindElement(a, array)}");
+
+// array1(array);  
+// Console.WriteLine(String.Join(",", array1(array)));
+
+int[] GetArray(int count, int minValue, int maxValue) // Обьявил условия для массива в методе 1. длина массива 2. мин. значение и макс.значение.
+{
+    int [] array = new int[count]; // объявил массив array и сказал что он будет равен count то есть его длинне.
+
+    for (int i = 0; i < count; i++) // запустил цикл for (для) внес переменные цикла и задал условия  
+    {
+        array[i] = new Random(). Next (minValue, maxValue + 1); // условия заполнения цикла
+    }
+
+    return array; // возврат 
+}
+ bool FindElement ( int digit, int [] array )
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( array [i]== a ) return true;
+    }   
+    return false;
+}
