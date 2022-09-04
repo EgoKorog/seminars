b1ea1b81a2b8e2966eeee16888317a5b76414c9d
EgoKorog <107405597+EgoKorog@users.noreply.github.com>
2022-09-04 14:34:11 +0700

    ДЗ упр 1 семинар 7

---
+++ HomeWork/HomeWork_Seminar_7/Ex_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+// table[1,2] = "Число";
+
+void PrintArray(double [,] tabl)
+{
+    for (int rows =0; rows < tabl.GetLength(0); rows++)
+    {
+        for (int colums =0; colums < tabl.GetLength(1); colums++)
+        {
+            Console.Write($"{tabl[rows, colums]}");
+        }
+    Console.WriteLine();    
+    }
+
+}
+
+void FillArray(double [,] tabl1)
+{
+    for (int rows =0; rows < tabl1.GetLength(0); rows++)
+    {
+        for (int colums =0; colums < tabl1.GetLength(1); colums++)
+        {
+            tabl1[rows,colums]= new Random().Next(-9,10);
+        }
+    Console.WriteLine();    
+    }
+}
+double [,] table = new double[3,4]; 
+PrintArray(table);
+FillArray(table);
+PrintArray(table);
